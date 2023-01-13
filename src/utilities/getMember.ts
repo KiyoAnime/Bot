4cee008e478219802df2caa3c70b09cb61e2683d
TheFallenSpirit <thefallenspirit@outlook.com>
2023-01-13 14:05:06 -0800

    TypeScript is my worst enemy

--- src/utilities/getMember.ts
+++ src/utilities/getMember.ts
@@ -1,9 +1,9 @@
-import { CommandInteraction, GuildMember } from "discord.js";
+import { CommandInteraction, GuildMember, InteractionResponse } from "discord.js";
 
 export default async (interaction: CommandInteraction, member: any): Promise<GuildMember|undefined> => {
     if (member) {
         if (interaction.guild?.members.cache.has(member.id)) {
             return interaction.guild?.members.cache.get(member.id)!;
-        } else interaction.reply({ content: 'Invalid member specified.', ephemeral: true });
-    } else interaction.reply({ content: 'Invalid member specified.', ephemeral: true });
+        }
+    }
 };
