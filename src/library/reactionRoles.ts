792c801a8e8e9e7d9abae9546aaf910cfc6d4d3c
TheFallenSpirit <thefallenspirit@outlook.com>
2023-01-24 21:12:55 -0800

    Fuck reaction roles

--- src/library/reactionRoles.ts
+++ src/library/reactionRoles.ts
@@ -4,16 +4,25 @@ import RRG from "@/models/ReactionRoleGroup";
 import { GuildMemberRoleManager, StringSelectMenuInteraction } from "discord.js";
 
 export default async (client: Client, interaction: StringSelectMenuInteraction) => {
-    if (!await RRG.exists({})) return;
+    if (!await RRG.exists({})) return console.log('no rrg');
     await interaction.deferUpdate();
+    const add: string[] = [];
     const opts = interaction.component.options;
-    for (const opt of opts) {
-        const role = await RR.findById(parseInt(opt.value.slice(-10)));
+    for (const value of interaction.values) {
+        const role = await RR.findById(parseInt(value.slice(-10)));
         if (!role) return;
-        if (interaction.values.includes(opt.value)) {
-            (interaction.member?.roles as GuildMemberRoleManager).add(role.role, 'Automated Action: Reaction Roles').catch(() => {});
-        } else {
-            (interaction.member?.roles as GuildMemberRoleManager).remove(role.role, 'Automated Action: Reaction Roles').catch(() => {});
+        await add.push(value);
+        if (!(interaction.member?.roles as GuildMemberRoleManager).cache.has(role.role)) {
+            await (interaction.member?.roles as GuildMemberRoleManager).add(role.role, 'Automated Action: Reaction Roles');
         }
     };
+    for (const opt of opts) {
+        if (!add.includes(opt.value)) {
+            const role = await RR.findById(parseInt(opt.value.slice(-10)));
+            if (!role) return;
+            if ((interaction.member?.roles as GuildMemberRoleManager).cache.has(role.role)) {
+                await (interaction.member?.roles as GuildMemberRoleManager).remove(role.role, 'Automated Action: Reaction Roles');
+            }
+        };
+    }
 };
